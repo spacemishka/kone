33f54cd7b672be86c07820a958c0b461a077f1c2
Peter Boelke <peter.boelke@biotronik.com>
2025-05-15 12:34:56 +0200

    mmh

--- boards/arm/nice_nano_v2_clear_bt/board.cmake
+++ boards/arm/nice_nano_v2_clear_bt/board.cmake
@@ -1,2 +1,4 @@
-# Inherit from the base nice!nano v2 board
-include(${ZEPHYR_BASE}/boards/arm/nice_nano_v2/board.cmake)
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
